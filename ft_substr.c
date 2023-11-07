4fcc87ce29e4d63bcc8889d85203f59cd03379eb
Antoine Verin <24255551+VerinAntoine@users.noreply.github.com>
2023-11-07 11:01:48 +0100

    fix substr

--- ft_substr.c
+++ ft_substr.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ft_substr.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: averin <averin@student.42.fr>              +#+  +:+       +#+        */
+/*   By: antoine <antoine@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/11 13:53:57 by averin            #+#    #+#             */
-/*   Updated: 2023/08/11 14:10:07 by averin           ###   ########.fr       */
+/*   Updated: 2023/11/07 10:58:01 by antoine          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,10 +15,23 @@
 char	*ft_substr(char const *s, unsigned int start, size_t len)
 {
 	char	*r;
+	size_t	slen;
+	size_t	rlen;
 
-	r = (char *) ft_calloc(len + 1, sizeof(char));
+	slen = ft_strlen(s);
+	if (slen < start)
+	{
+		r = ft_calloc(1, sizeof(char));
+		*r = '\0';
+		return (r);
+	}
+	if (len > slen - start)
+		rlen = slen - start;
+	else
+		rlen = len;
+	r = (char *) ft_calloc(rlen + 1, sizeof(char));
 	if (!r)
 		return (NULL);
-	ft_strlcpy(r, s + start, len + 1);
+	ft_strlcpy(r, s + start, rlen + 1);
 	return (r);
 }
